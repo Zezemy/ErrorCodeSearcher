badadc8668693e29d410f06879047e1bcbfe6a57
Zeynep Emine Yalçın <zeyalcin@stu.okan.edu.tr>
2024-08-05 21:25:21 +0300

    SQL CreateTable scripti eklendi.

---
+++ SQLScripts/CreateTable.sql
@@ -0,0 +1,14 @@
+USE XfsDataDb
+GO
+DROP TABLE ErrorDatas
+CREATE TABLE ErrorDatas (
+	Id bigint IDENTITY PRIMARY KEY,
+	Code varchar(50),
+	[Description] nvarchar(4000),
+	Category varchar(100),
+	DeviceClassName varchar(200) null,
+	CreatedBy varchar(100) null,
+	CreateDate datetime null,
+	UpdatedBy varchar(100) null,
+	UpdateDate datetime null
+)
